b6fd0ece231cde571ad208be2fd0e0f32c039a9f
Andrew Villadsen <andrew.villadsen@gmail.com>
2017-09-16 16:07:18 -0500

    basic read_line and write_string; shell echoes input

--- src/main.c
+++ src/main.c
@@ -1,7 +1,34 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
 
-#include <stdio.h>
+#define INPUT_BUFFER_SIZE 1024
 
-void main() {
-  printf("Hello World!\n");
+char *read_line() {
+  char buffer[INPUT_BUFFER_SIZE];
+
+  // TODO handle errors
+  size_t length = read(0, buffer, INPUT_BUFFER_SIZE);
+
+  // TODO handle multiple lines at once?
+  
+  char *line = malloc(length + 1);
+  memcpy(line, buffer, length);
+
+  return line;
 }
 
+void write_string(int fd, char *string) {
+  size_t length = strlen(string);
+
+  // TODO handle errors
+  write(1, string, length);
+}
+
+void main() {
+  while(true) {
+    char *line = read_line();
+    write_string(1, line);
+  }
+}
